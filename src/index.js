e1b078c3fe50882bb5f38e820ce69ea788f7ca44
ngodinhtri <ngodinhtri@outlook.com>
2021-08-08 21:03:27 +0700

    create routes & controllers folders

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const path = require("path");
-const morgan = require("morgan")
+const express = require('express')
+const exphbs = require('express-handlebars')
+const morgan = require('morgan')
+const route = require('./routes')
+const path = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 //HTTP logger
-app.use(morgan("tiny"))
+app.use(morgan('tiny'))
 
 //Template engine handlebars
 app.engine('hbs', exphbs({
@@ -17,14 +22,8 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources\\views'))
 
-//Route
-app.get("/", (req, res) => {
-  res.render('home')
-})
-app.get("/news", (req, res) => {
-  res.render('news')
-})
-
+//Routing
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
